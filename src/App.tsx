52a84dc29e566c0ac1a7f595e89ecf849138f6d4
prasadk <prasadkatepk@gmail.com>
2025-04-27 12:18:36 +0530

    feat: setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="*" element={<>Page not found!</>} />
+      <Route path="/" element={<>Home Page</>} />
+      <Route path="/login" element={<>Login Page</>} />
+    </Routes>
+  );
 }
 
 export default App;
