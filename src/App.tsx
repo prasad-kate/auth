73338e6f8ecc2280e39f7e38b4ae2906c00eed80
prasadk <prasadkatepk@gmail.com>
2025-04-27 13:57:44 +0530

    feat: add login page with api integrations

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./components/login";
 
 function App() {
   return (
     <Routes>
       <Route path="*" element={<>Page not found!</>} />
       <Route path="/" element={<>Home Page</>} />
-      <Route path="/login" element={<>Login Page</>} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
